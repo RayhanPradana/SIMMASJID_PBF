80b3c5edda4780b9ec0b09d2a50f4cc0a45cc896
JafarMalikIbrahim <jakfarjakpeng007@gmail.com>
2025-03-12 15:42:29 +0700

    jadwal kegiatan

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BeritaController;
-
+use App\Http\Controllers\Api\JadwalController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,5 +15,10 @@ Route::get('beritaa/{berita}', [BeritaController::class, 'show']);
 Route::put('beritaa/{berita}', [BeritaController::class, 'update']);
 Route::delete('beritaa/{berita}', [BeritaController::class, 'destroy']);
 
-
+//Jadwal Kegiatan
+Route::get('jadwal', [JadwalController::class, 'index']);
+Route::post('jadwal', [JadwalController::class, 'store']);
+Route::get('jadwal/{jadwal}', [JadwalController::class, 'show']);
+Route::put('jadwal/{jadwal}', [JadwalController::class, 'update']);
+Route::delete('jadwal/{jadwal}', [JadwalController::class, 'destroy']);
 
