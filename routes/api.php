240f2ca6a73ae6c2e4515b794ac43e3bf9ee169d
eywidyanti <eywidyanti@gmail.com>
2025-06-03 03:20:19 +0000

    update konfigurasi

--- routes/api.php
+++ routes/api.php
@@ -23,9 +23,6 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('jadwals', [JadwalController::class, 'index']);
 Route::get('beritas', [BeritaController::class, 'index']);
 
-Route::get('jadwals', [JadwalController::class, 'index']);
-Route::get('beritas', [BeritaController::class, 'index']);
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
